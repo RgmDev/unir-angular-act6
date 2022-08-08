04baea622742b8b22db55f01c3ab382da1aa028d
rgonzalez <rgonzalez@mkdautomotive.com>
2022-08-08 09:53:11 +0200

    Criterio 2 - Creación de rutas y componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { NewUserComponent } from './components/new-user/new-user.component';
+import { UpdateUserComponent } from './components/update-user/update-user.component';
+import { UserComponent } from './components/user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/home'},
+  { path:'home', component: HomeComponent },
+  { path:'user/:userId', component: UserComponent },
+  { path:'newuser', component: NewUserComponent },
+  { path:'updateuser/:userId', component: UpdateUserComponent },
+  { path: '**', redirectTo: '/home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { UserComponent } from './components/user/user.component';
+import { NewUserComponent } from './components/new-user/new-user.component';
+import { UpdateUserComponent } from './components/update-user/update-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UserComponent,
+    NewUserComponent,
+    UpdateUserComponent,
   ],
   imports: [
     BrowserModule,
